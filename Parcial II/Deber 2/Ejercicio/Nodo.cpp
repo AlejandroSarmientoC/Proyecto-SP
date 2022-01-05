b9b314ae350b0d91706001e65358b3f612a59f4a
AlejandroSarmientoC <basarmiento@espe.edu.ec>
2022-01-04 23:32:35 -0500

    deber 2

---
+++ Parcial II/Deber 2/Ejercicio/Nodo.cpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "Nodo.h"
+#include <iostream>
+#include <stdio.h>
+
+void Nodo::setDato(int _dato){
+ dato = _dato;
+}
+void Nodo::setIzquierda(Nodo* _izquierda){
+ izquierda = _izquierda;
+ }
+void Nodo::setDerecha(Nodo* _derecha){
+ derecha = _derecha;
+}
+int Nodo::getDato(){
+ return dato;
+}
+Nodo*& Nodo::getIzquierda(){
+ return izquierda;
+}
+Nodo*& Nodo::getDerecha(){
+ return derecha;
+}
